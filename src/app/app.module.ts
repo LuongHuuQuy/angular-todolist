d9d65486ac95f7d8edd7a33317e67ddd4868c9ef
LuongHuuQuy <lluongqquy.2001@gmail.com>
2022-08-31 11:02:21 +0700

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ParentModule} from './components/parent/parent.module';
+
+import {ParentModule} from './components/parent/parent.module'
+import {ChildModule} from './components/child/child.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule,ParentModule],
+  imports: [BrowserModule, FormsModule, ParentModule,ChildModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/parent/parent.module.ts
+++ src/app/components/parent/parent.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChildComponent } from '../child/child.component';
 import { ParentComponent } from './parent.component';
+import {ChildModule} from '../child/child.module'
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [ChildComponent, ParentComponent],
+  imports: [CommonModule,ChildModule],
+  declarations: [ParentComponent],
   exports: [ParentComponent],
 })
 export class ParentModule {}
